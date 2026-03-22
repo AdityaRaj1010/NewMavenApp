1200c879378b24d80865872097113315184f2222
AdityaRaj1010 <adityaraj9061811@gmail.com>
2026-03-22 15:43:09 +0530

    MavenJenkinsPipeline

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Who's gonna carry the boats... and the logs" );
+    }
+}
